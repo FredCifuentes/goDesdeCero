de281428943e5cbb81a608980dec2efc98a609c0
1075807 <fredi.cifuenter@elektra.com.mx>
2023-04-29 16:24:59 -0600

    Usando condicionales

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/fredcifuentes/goDesdeCero/variables"
 )
@@ -10,4 +11,18 @@ func main() {
 	Estado, Nombre := variables.ConvertirTexto(1200)
 	fmt.Println(Estado)
 	fmt.Println(Nombre)
+
+	if os := runtime.GOOS; os == "Linux." {
+		fmt.Println("es linux")
+	} else {
+		fmt.Println("Es windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "Linux.":
+		fmt.Println("es Linux")
+	default:
+		fmt.Println("Es Windows en el swithc")
+
+	}
 }
